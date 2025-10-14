14490b8f79b1e52157020e52000b467eb924b51c
ErikShehu <Eriks67@nycstudents.net>
2025-10-14 11:50:55 -0400

    he didnt logout

---
+++ src/WordMatch.java
@@ -0,0 +1,16 @@
+public class WordMatch
+{
+    private String secret;
+    public WordMatch( String word )
+    {
+
+    }
+        public int scoreGuess( String guess )
+        {
+            return 0;
+        }
+        public String findBetterGuess( String guess1, String guess2 )
+        {
+            return null;
+        }
+}
