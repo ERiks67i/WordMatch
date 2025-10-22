ac98bcc9298f6aedf99591d15c8cdb724fc80f6e
ErikShehu <Eriks67@nycstudents.net>
2025-10-22 11:40:39 -0400

    final

--- src/WordMatch.java
+++ src/WordMatch.java
@@ -3,14 +3,22 @@ public class WordMatch
     private String secret;
     public WordMatch( String word )
     {
-
+        secret = word;
     }
-        public int scoreGuess( String guess )
+    public int scoreGuess( String guess )
+    {
+        int score = 0;
+        for (int i = 0; i <= secret.length() - guess.length(); i++)
         {
-            return 0;
+            if (secret.substring(i, i + guess.length()).equals(guess))
+                score++;
         }
-        public String findBetterGuess( String guess1, String guess2 )
-        {
-            return null;
-        }
+        return score * guess.length()*guess.length();
+    }
+    public String findBetterGuess( String guess1, String guess2 )
+    {
+        if (scoreGuess(guess1) > scoreGuess(guess2)) return guess1;
+        if (scoreGuess(guess2) > 0) return guess1;
+        return guess2;
+    }
 }
